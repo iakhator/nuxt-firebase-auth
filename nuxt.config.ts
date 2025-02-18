c8102bbb14863a4633dae04dde7cc2cf1f29b31a
iakhator <iakhator@gmail.com>
2025-02-18 18:48:35 +0100

    omit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,16 @@ export default defineNuxtConfig({
   //   '/': { ssr: false },
   // },
   runtimeConfig: {
-    FIREBASE_SERVICE_ACCOUNT: process.env.FIREBASE_SERVICE_ACCOUNT,
-    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    public: {
+      FIREBASE_DATABASE_URL: process.env.DATABASE_URL,
+      API_KEY: process.env.API_KEY,
+      AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+      PROJECT_ID: process.env.PROJECT_ID,
+      STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+      MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+      APP_ID: process.env.APP_ID,
+      MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+    },
   },
   modules: ['@pinia/nuxt'],
 })
